5ab883d298a70abefa9b2e6b1c8f66e8530b12d9
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-17 07:49:16 -0400

    Create function names and comments

---
+++ javascript.js
@@ -0,0 +1,21 @@
+
+
+
+function game(){
+    for (let i =0; i < 5; i++){
+        //insert code to call playRound and keep score and return results
+    }
+}
+
+function computerPlay(){
+    //insert code to randomly return computers rock paper or scissor return as const computerSelection = computerPlay
+}
+
+function playerPlay(){
+    //use prompt() to get input from user and return case insensitive and check for incorrect answers const playerSelection = playerPlay
+}
+
+
+function playRound(playerSelection, computerselection){
+    //insert code to compare two inputs and return "You Lose! Paper beats Rock"
+}
