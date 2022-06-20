88665911373cc94b7a7d6f4ee08342bcea1b95c9
Inc0gnit076 <kmacinnis76@gmail.com>
2022-06-20 19:14:11 -0400

    complete playround function and game function to print to console

--- javascript.js
+++ javascript.js
@@ -1,15 +1,19 @@
 
-//game();
+game();
 //playerPlay();
-console.log(computerPlay());
+//console.log(computerPlay());
+//let playerSelection = playerPlay();
+//let computerselection = computerPlay();
+//playRound(playerSelection, computerselection);
+
 function game(){
     for (let i =0; i < 5; i++){ //change this eventually to run until one team scores 3 wins
         //insert code to call playRound and keep score and return results
         let playerSelection = playerPlay();
         let computerselection = computerPlay();
-        playRound();
+        playRound(playerSelection, computerselection);
     }
-}
+} //this works but just prints to the console
 
 function computerPlay(){
     const rndInt = Math.floor(Math.random() * 3); //gets random number between 0 and 2
@@ -46,5 +50,38 @@ function playerPlay(){
 
 
 function playRound(playerSelection, computerselection){
+    if(playerSelection == "rock") {
+        if (computerselection == "rock"){
+            console.log("tie");
+        }
+        else if (computerselection == "paper"){
+            console.log("You Lose! Paper beats Rock!");
+        }
+        else if (computerselection == "scissors"){
+            console.log("You Win! Rock beats scissors!");
+        }
+    }
+    if (playerSelection == "paper"){
+        if (computerselection == "paper"){
+            console.log("tie");
+        }
+        else if (computerselection == "rock"){
+            console.log("You Win! Paper beats Rock!");
+        }
+        else if (computerselection == "scissors"){
+            console.log("You Lose! scissors beats paper!");
+        }
+    }
+    if (playerSelection == "scissors"){
+        if (computerselection == "scissors"){
+            console.log("tie");
+        }
+        else if (computerselection == "paper"){
+            console.log("You Win! Scissors beats Paper!");
+        }
+        else if (computerselection == "rock"){
+            console.log("You Lose! Rock beats Scissors!");
+        }
+    }
     //insert code to compare two inputs and return "You Lose! Paper beats Rock"
-}
+} // this works but just prints to the console
